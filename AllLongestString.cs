791da692c13f86ad604a2af17eb5e6a9281a6350
amandamata <amanda.mata@live.com>
2023-06-14 17:16:07 -0300

    Adds first try code

--- AllLongestString.cs
+++ AllLongestString.cs
@@ -24,3 +24,32 @@ public static class AllLongestString
         return tempList.ToArray();
     }
 }
+// var totalItemsInArray = inputArray.Length;
+// var totalCharCount = 0;
+
+// foreach (string input in inputArray)
+//     totalCharCount += input.Count();
+
+// double average = (double)totalCharCount / totalItemsInArray;
+// int roundedValue = (int)Math.Ceiling(average);
+// int howManyString = 0;
+
+// foreach (string input in inputArray)
+// {
+//     if (input.Count() >= roundedValue)
+//         howManyString++;
+// }
+
+// int count = 0;
+// string[] newArray = new string[howManyString];
+
+// foreach (string input in inputArray)
+// {
+//     if (input.Count() >= roundedValue)
+//     {
+//         newArray[count] = input;
+//         count++;
+//     }
+// }
+
+// return newArray;
