d56424a8a59396830f3dfd63b4aff9c1732e525c
amandamata <amanda.mata@live.com>
2023-06-19 14:26:48 -0300

    CompanyBotStrategy improvements

--- CompanyBotStrategy.cs
+++ CompanyBotStrategy.cs
@@ -4,35 +4,25 @@ public static class CompanyBotStrategy
 {
     public static double solution(int[][] trainingData)
     {
-        // for loop
-        // check if the second colun value is equals 1
-        // add a counter to the check
-        // sum the values
-
         int count = 0;
         int sum = 0;
-        double result = 0;
 
-        for (var i = 0; i < trainingData.Length; i++)
+        foreach (var data in trainingData)
         {
-            for (var j = 0; j < trainingData[0].Length; j++)
+            int answerTime = data[0];
+            int correcness = data[1];
+
+            if (correcness == 1)
             {
-                if (j == 1)
-                {
-                    if (trainingData[i][j] != 0 && trainingData[i][j] != -1)
-                    {
-                        count++;
-                        sum += trainingData[i][j - 1];
-                    }
-                }
-                Console.WriteLine($"i[{i}]j[{j}] = {trainingData[i][j]}");
+                sum += answerTime;
+                count++;
             }
         }
 
-        if (sum == 0 || count == 0)
+        if (count == 0)
             return 0;
 
-        result = ((double)sum / count);
-        return result;
+        double average = (double)sum / count;
+        return average;
     }
 }
